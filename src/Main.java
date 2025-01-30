417458c12d372e857c3e231185712a2fa077b6b9
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-01-30 16:54:23 -0500

    Complete

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        double startingBalance = 5000.0;
+        double INTEREST_RATE = 0.17;
+        double oneMonthInterest = 0;
+        double twoMonthInterest = 0;
+
+        oneMonthInterest = Math.round(startingBalance * INTEREST_RATE);
+        twoMonthInterest = oneMonthInterest * INTEREST_RATE;
+
+        System.out.println("Your interest due after one month is $" + oneMonthInterest + ".");
+        System.out.println("Your interest due after two months is $" + twoMonthInterest + ".");
+
     }
 }
