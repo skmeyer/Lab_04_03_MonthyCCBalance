c688f3f44f43fe8ab8c369bf78fbc5290a185fbf
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-01-30 16:57:58 -0500

    Complete

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ public class Main
     {
         double startingBalance = 5000.0;
         double INTEREST_RATE = 0.17;
-        double oneMonthInterest = 0;
-        double twoMonthInterest = 0;
 
-        oneMonthInterest = Math.round(startingBalance * INTEREST_RATE);
-        twoMonthInterest = oneMonthInterest * INTEREST_RATE;
+        double oneMonthInterest = Math.round(startingBalance * INTEREST_RATE);
+        double twoMonthInterest = oneMonthInterest * INTEREST_RATE;
 
         System.out.println("Your interest due after one month is " + oneMonthInterest + " dollars.");
         System.out.println("Your interest due after two months is " + twoMonthInterest + " dollars.");
